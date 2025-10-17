630b22454abad23f25844e7ab7fbe62056ed444e
firas1996 <firas.hm.1996@gmail.com>
2025-10-17 14:21:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express"); // Importation du framework Express
+const mongoose = require("mongoose"); // Importation de Mongoose pour interagir avec MongoDB
+const dotenv = require("dotenv"); // Importation de dotenv pour gérer les variables d'environnement
+
+// Configuration de dotenv pour charger les variables d'environnement depuis le fichier .env
+dotenv.config({ path: "./.env" });
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => {
+    console.log("Connection to the DB secured !!!"); // Message de succès si la connexion est établie
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const app = express(); // Création d'une instance de l'application Express
+const port = 1234; // Définition du port d'écoute du serveur
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+}); // Démarrage du serveur et affichage d'un message de confirmation
---
+++ models/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide your name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 8,
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 8,
+    validate: function (cPass) {
+      return cPass === this.password;
+    },
+    message: "Passwords are not the same",
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
